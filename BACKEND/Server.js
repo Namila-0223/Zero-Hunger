a87d4523208e5fa625e08290f725cf6718082929
BIMSARA26D <88665149+BIMSARA26D@users.noreply.github.com>
2023-03-25 16:40:49 +0530

    updated logger script

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
+import logger from "./src/utils/logger.js";
 
 
 
@@ -16,7 +17,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.listen(port, () => {
-   
+    logger.info("this is a test")
     console.log(`Server is up and running on port ${port}`);
 });
 
