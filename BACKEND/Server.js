3f30574855f758e71c7e0a180613969f2d1119c3
BIMSARA26D <88665149+BIMSARA26D@users.noreply.github.com>
2023-03-25 10:32:37 +0530

    logger scripts created

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -1,1 +1,26 @@
-console.log("hi")
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+
+
+const app = express();
+const port = process.env.port || "8090";
+
+app.use(cors());
+app.use(express.json({limit:"20mb"}));
+
+app.get("/", (req, res, next) => {
+    res.send("<h1>Welcome</h1>");
+    next();
+});
+
+app.listen(port, () => {
+   
+    console.log(`Server is up and running on port ${port}`);
+});
+
+
+
+
+
